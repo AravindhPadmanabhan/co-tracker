6069ab8467ec4c34e87dda397c3847661239afed
AravindhPadmanabhan <apadmanabhan@student.ethz.ch>
2025-02-07 13:00:23 +0100

    Add query update facility in cotracker

--- cotracker/predictor_update.py
+++ cotracker/predictor_update.py
@@ -8,7 +8,7 @@ import torch
 import torch.nn.functional as F
 
 from cotracker.models.core.model_utils import smart_cat, get_points_on_a_grid
-from cotracker.models.build_cotracker import build_cotracker
+from cotracker.models.build_cotracker_update import build_cotracker
 
 
 class CoTrackerPredictor(torch.nn.Module):
@@ -230,47 +230,31 @@ class CoTrackerOnlinePredictor(torch.nn.Module):
         video_chunk,
         is_first_step: bool = False,
         queries: torch.Tensor = None,
-        grid_size: int = 5,
-        grid_query_frame: int = 0,
-        add_support_grid=False,
+        removed_indices = None,
     ):
         B, T, C, H, W = video_chunk.shape
-        # Initialize online video processing and save queried points
-        # This needs to be done before processing *each new video*
+        # Initialize online video processing
         if is_first_step:
             self.model.init_video_online_processing()
-            if queries is not None:
-                B, N, D = queries.shape
-                self.N = N
-                assert D == 3
-                queries = queries.clone()
-                queries[:, :, 1:] *= queries.new_tensor(
-                    [
-                        (self.interp_shape[1] - 1) / (W - 1),
-                        (self.interp_shape[0] - 1) / (H - 1),
-                    ]
-                )
-                if add_support_grid:
-                    grid_pts = get_points_on_a_grid(
-                        self.support_grid_size, self.interp_shape, device=video_chunk.device
-                    )
-                    grid_pts = torch.cat(
-                        [torch.zeros_like(grid_pts[:, :, :1]), grid_pts], dim=2
-                    )
-                    queries = torch.cat([queries, grid_pts], dim=1)
-            elif grid_size > 0:
-                grid_pts = get_points_on_a_grid(
-                    grid_size, self.interp_shape, device=video_chunk.device
-                )
-                self.N = grid_size**2
-                queries = torch.cat(
-                    [torch.ones_like(grid_pts[:, :, :1]) * grid_query_frame, grid_pts],
-                    dim=2,
-                )
-            
-            self.queries = queries
             return (None, None, None)
-
+            
+        if queries is None:
+            raise ValueError("No queries given")
+        
+        # Scale and assign queries
+        B, N, D = queries.shape
+        self.N = N
+        assert D == 3
+        queries = queries.clone()
+        queries[:, :, 1:] *= queries.new_tensor(
+            [
+                (self.interp_shape[1] - 1) / (W - 1),
+                (self.interp_shape[0] - 1) / (H - 1),
+            ]
+        )    
+        self.queries = queries
+            
+        # Reshape video
         video_chunk = video_chunk.reshape(B * T, C, H, W)
         video_chunk = F.interpolate(
             video_chunk, tuple(self.interp_shape), mode="bilinear", align_corners=True
@@ -279,14 +263,11 @@ class CoTrackerOnlinePredictor(torch.nn.Module):
             B, T, 3, self.interp_shape[0], self.interp_shape[1]
         )
 
+        # CoTrackerThreeOnline inference
         tracks, visibilities, confidence, __ = self.model(
-            video=video_chunk, queries=self.queries, iters=6, is_online=True
+            video=video_chunk, queries=self.queries, iters=6, is_online=True, removed_indices=removed_indices
         )
-        if add_support_grid:
-            tracks = tracks[:,:,:self.N]
-            visibilities = visibilities[:,:,:self.N]
-            confidence = confidence[:,:,:self.N]
-            
+
         visibilities = visibilities * confidence
         thr = 0.6
         return (
